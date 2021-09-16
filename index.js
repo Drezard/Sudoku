b5cc787711075cc38c8cdb62777a82d02aff10e3
Dunice Practice <dunicepractice@Mac-mini-Dunice-10.local>
2021-09-16 13:07:52 +0300

    Добавил в Судоку правки

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ function pressed() {
 }
 button.addEventListener('click', pressed);
 
-function getRandomInt(min, max) {
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const cells = [];
+const possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function generateGameField() {
 
@@ -24,22 +25,41 @@ function generateGameField() {
 
   for (let rowIndex = 0; rowIndex < rowNumber; rowIndex += 1) {
     for (let colIndex = 0; colIndex < colNumber; colIndex += 1) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      gameField.append(cell);
+      const cellElement = document.createElement('div');
+      cellElement.classList.add('cell');
+      gameField.append(cellElement);
 
-      const number = getRandomInt(1, 9); //generate
+      const numbersInRow = cells.filter((item) => item.row === rowIndex).map((item) => item.number);
+      const numbersInColumn = cells.filter((item) => item.col === colIndex).map((item) => item.number);
 
 
-      cell.textContent = number;
+      const sqrIndex = Math.floor(colIndex / 3) + Math.floor(rowIndex / 3) * 3;
+
+      // const numbersInColumnAndRow = cells.filter((item) => item.col&row === colIndex).map((item) => item.number)
+      const numbersInSqr = cells.filter((item) => item.sqr === sqrIndex).map((item) => item.number); 
       
-      cells.push({
+      const numberList = possibleNumbers.filter((numb) => {
+        return !(numbersInRow.includes(numb) || numbersInColumn.includes(numb) || numbersInSqr.includes(numb));
+      })
+
+      const rand = getRandomIntInclusive(0, numberList.length - 1);
+      console.log(numberList, rand, numberList[rand]);
+
+      const number = numberList[rand]; //generate
+
+
+      cellElement.textContent = number;
+
+      const cell = {
         row: rowIndex,
+        sqr: sqrIndex,
         col: colIndex,
-        el: cell,
+        el: cellElement,
         number: number,
         isShowed: true,
-      });
+      };
+      
+      cells.push(cell);
 
 
     }
